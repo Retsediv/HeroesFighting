f63431eed037076097dc4712fff83c6ce9158167
andrew <retsediv1@gmail.com>
2017-11-16 01:27:20 +0200

    CharacterFactory tests

---
+++ src/test/java/CharacterFactoryTest.java
@@ -0,0 +1,43 @@
+import com.herofight.Character;
+import com.herofight.CharacterFactory;
+import junit.framework.TestCase;
+import org.reflections.Reflections;
+
+import java.util.Set;
+
+public class CharacterFactoryTest extends TestCase {
+    Set<Class<? extends Character>> childs;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        Reflections reflections = new Reflections("com.herofight");
+        this.childs = reflections.getSubTypesOf(Character.class);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testFactoryCreation() throws Exception {
+        CharacterFactory characterFactory = new CharacterFactory(this.childs);
+
+        assertEquals(CharacterFactory.class, characterFactory.getClass());
+    }
+
+    public void testFactoryGettingTheCharacters() throws Exception {
+        CharacterFactory characterFactory = new CharacterFactory(this.childs);
+
+        assertEquals(characterFactory.getCharacters(), this.childs);
+    }
+
+    public void testCharacterCreation() throws Exception {
+        CharacterFactory characterFactory = new CharacterFactory(this.childs);
+
+        assertTrue(characterFactory.createCharacter() instanceof Character);
+        assertTrue(this.childs.contains(characterFactory.createCharacter().getClass()));
+    }
+
+}
